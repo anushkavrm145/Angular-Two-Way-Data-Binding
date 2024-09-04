6bb59f1c7b8632e0c4fc2831b6f8bdd31ed4c717
anushkavrm145 <anushkavrm145@gmail.com>
2024-09-04 17:22:43 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngIntro-Form';
+
+  postTitle:string='';
+  postDetails:string='';
+  imageUrl:string='';
+  postUrl:string='';
+  addBackground: boolean=false;
 }
